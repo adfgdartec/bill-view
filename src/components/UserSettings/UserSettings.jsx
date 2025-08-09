f615338c85e9464f2f155a1056f4cc2becb6c90b
gadeharshithreddy <gadeharshithreddy@gmail.com>
2025-08-09 14:30:40 -0500

    Fixs settings CSS

--- src/components/UserSettings/UserSettings.jsx
+++ src/components/UserSettings/UserSettings.jsx
@@ -18,59 +18,62 @@ export default function UserSettings() {
 
     return (
         <div className={styles.container}>
-            <div className={styles.containerSegment}>
-                <p>Profile Info:</p>
-                <Avatar>H</Avatar>
-                <p>Change Profile Photo</p>
-            </div>
-
-            <div className={styles.containerSegment}>
-                <p>Username</p>
-                <TextField
-                    className={styles.textField}
-                    label="Enter your username"
-                    value="User Current Username"
-                    type="username"
-                    size="small"
-                    variant="outlined"
-                    sx={{
-                        marginBottom: "30px"
-                    }}
-                />
-
-                <p>Email</p>
-                <TextField
-                    className={styles.textField}
-                    label="Change your password"
-                    type="User Current Email"
-                    size="small"
-                    variant="outlined"
-                    sx={{
-                        marginBottom: "30px"
-                    }}
-                />
-
-                {/* Future Features */}
-                {/* <ChangeColorPicker /> */}
-
-            </div>
-
-            <div className={styles.containerSegment}>
-                <p>Notification Settings</p>
-                <div className={styles.flex}>
-                    <Switch />
-                    <p>Send Emails</p>
+            <div className={styles.settingsContainer}>
+                <div className={styles.containerSegment}>
+                    <p className={styles.titleText}>Profile Info:</p>
+                    <Avatar className={styles.avatar}>H</Avatar>
+                    <p>Change Profile Photo</p>
                 </div>
-                <div className={styles.flex}>
-                    <Switch />
-                    <p>Send Messages</p>
-                </div>
-            </div>
 
-            <div className={styles.containerSegment}>
-                <p>Security:</p>
-                <ActionButton content="Change Password" onClick={handlePasswordChange} />
-                <ActionButton content="Delete Account" className={styles.deleteAccount} onClick={handleDeleteAccount} />
+                <div className={styles.containerSegment}>
+                    <p className={styles.titleText}>Account Settings</p>
+                    <p>Username</p>
+                    <TextField
+                        className={styles.textField}
+                        label="Enter your username"
+                        value="User Current Username"
+                        type="username"
+                        size="small"
+                        variant="outlined"
+                        sx={{
+                            marginBottom: "30px"
+                        }}
+                    />
+
+                    <p>Email</p>
+                    <TextField
+                        className={styles.textField}
+                        label="Change your password"
+                        type="User Current Email"
+                        size="small"
+                        variant="outlined"
+                        sx={{
+                            marginBottom: "30px"
+                        }}
+                    />
+
+                    {/* Future Features */}
+                    {/* <ChangeColorPicker /> */}
+
+                </div>
+
+                <div className={styles.containerSegment}>
+                    <p className={styles.titleText}>Notification Settings</p>
+                    <div className={styles.flex}>
+                        <Switch />
+                        <p>Send Emails</p>
+                    </div>
+                    <div className={styles.flex}>
+                        <Switch />
+                        <p>Send Messages</p>
+                    </div>
+                </div>
+
+                <div className={styles.containerSegment}>
+                    <p className={styles.titleText}>Security:</p>
+                    <ActionButton content="Change Password" onClick={handlePasswordChange} />
+                    <ActionButton content="Delete Account" className={styles.deleteAccount} onClick={handleDeleteAccount} />
+                </div>
             </div>
         </div>
     );
