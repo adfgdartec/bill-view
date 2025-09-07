6dbcfed5f4d2b97ce855b9b71ceb391c1a8c3f38
gadeharshithreddy <gadeharshithreddy@gmail.com>
2025-09-07 18:52:13 -0500

    Adds additions for features. Needs more editing in the future

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -6,9 +6,12 @@ import styles from './features.module.css';
 export default function Features(props) {
     return (
         <div className={styles.container}>
-            <img src={props.imageSource} alt={props.alt} />
-            <div>
-                
+            <div className={styles.leftContainer}>
+                <img className={styles.featureImage} src={props.imageSource} alt={props.alt} />
+            </div>
+            <div className={styles.rightContainer}>
+                <p className={styles.featureTitle}>This is our greatest feature.</p>
+                <p className={styles.featureDescription}>Here is where we can talk about something about this feature.</p>
             </div>
         </div>
     );
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -15,50 +15,63 @@ export default function Hero() {
     const rotatingText = ["Experience", "Clarity", "Impact", "Community"];
 
     return (
-        <motion.div 
-            className={styles.hero}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 3 }}
-        >
-            <FloatingPapers count={18} />
-            <div className={styles.centerDiv}>
-                <p className={styles.centerText}>The Best Designed</p>
-                <motion.div className={styles.innerCenterDiv}>
-                    <motion.p
-                        className={`${styles.animatedGradient} ${styles.importantText}`}
-                        initial={{ opacity: 0, y: 10 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        exit={{ opacity: 0, y: -10 }}
-                        transition={{ duration: 1, ease: "easeInOut" }}
-                    >
-                        Bill-Viewing
-                    </motion.p>
+        <div>
+            <motion.div 
+                className={styles.hero}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 3 }}
+            >
+                <FloatingPapers count={18} />
+                <div className={styles.centerDiv}>
+                    <p className={styles.centerText}>The Best Designed</p>
+                    <motion.div className={styles.innerCenterDiv}>
+                        <motion.p
+                            className={`${styles.animatedGradient} ${styles.importantText}`}
+                            initial={{ opacity: 0, y: 10 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: -10 }}
+                            transition={{ duration: 1, ease: "easeInOut" }}
+                        >
+                            Bill-Viewing
+                        </motion.p>
 
-                    <RotatingText
-                        texts={rotatingText}
-                        mainClassName={`px-2 sm:px-2 md:px-3 
-                                        bg-clip-text text-transparent 
-                                        ${styles.rotatingText}
-                                        overflow-hidden py-0.5 sm:py-1 md:py-2 
-                                        justify-center rounded-lg`}
-                        staggerFrom="last"
-                        initial={{ y: "100%", opacity: 0 }}
-                        animate={{ y: 0, opacity: 1 }}
-                        exit={{ y: "-20%", opacity: 0 }}
-                        staggerDuration={0.025}
-                        splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
-                        transition={{ type: "spring", damping: 30, stiffness: 200 }}
-                        rotationInterval={3000}
-                    />
-                </motion.div>
+                        <RotatingText
+                            texts={rotatingText}
+                            mainClassName={`px-2 sm:px-2 md:px-3 
+                                            bg-clip-text text-transparent 
+                                            ${styles.rotatingText}
+                                            overflow-hidden py-0.5 sm:py-1 md:py-2 
+                                            justify-center rounded-lg`}
+                            staggerFrom="last"
+                            initial={{ y: "100%", opacity: 0 }}
+                            animate={{ y: 0, opacity: 1 }}
+                            exit={{ y: "-20%", opacity: 0 }}
+                            staggerDuration={0.025}
+                            splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                            transition={{ type: "spring", damping: 30, stiffness: 200 }}
+                            rotationInterval={3000}
+                        />
+                    </motion.div>
 
-                <div className={styles.actionButtons}>
-                    <ActionButton location="/login" content="Login" className={styles.actionButton}/>
-                    <ActionButton location="/sign-up" content="Sign Up" className={styles.actionButton}/>
+                    <div className={styles.actionButtons}>
+                        <ActionButton location="/login" content="Login" className={styles.actionButton}/>
+                        <ActionButton location="/sign-up" content="Sign Up" className={styles.actionButton}/>
+                    </div>
                 </div>
-            </div>
-            
-        </motion.div>
+            </motion.div>
+            {/* Features section planning to add in the future */}
+            {/* <motion.div
+                className={styles.features}
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1, ease: "easeOut" }}
+                viewport={{ once: true, amount: 0.2 }} 
+            >
+                <p className={styles.text}>
+                    Bill-Viewing Centered for Teens
+                </p>
+            </motion.div> */}
+        </div>
     );
 }
